72c9cca7820013b64a37c47e17bfcb3c65f8967d
Yash Tewari <yashtewari1996@gmail.com>
2021-11-29 17:22:30 +0530

    Functional Kube API Fetcher with PodSecurityPolicy.

--- kubebeat/beater/kube.go
+++ kubebeat/beater/kube.go
@@ -1,11 +1,14 @@
 package beater
 
 import (
+	"context"
 	"fmt"
 	"time"
 
 	"github.com/elastic/beats/v7/libbeat/common/kubernetes"
 	"github.com/elastic/beats/v7/libbeat/logp"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/kubernetes/pkg/apis/policy"
 )
 
 const ()
@@ -17,22 +20,31 @@ type KubeFetcher struct {
 func NewKubeFetcher(kubeconfig string, interval time.Duration) (Fetcher, error) {
 	client, err := kubernetes.GetKubernetesClient(kubeconfig, kubernetes.KubeClientOptions{})
 	if err != nil {
-		return nil, fmt.Errorf("fail to get k8sclient client: %s", err.Error())
+		return nil, fmt.Errorf("could not get k8s client: %w", err)
 	}
 
 	logp.Info("Client initiated.")
+
+	psp, err := client.CoreV1().Pods("kube-system").List(context.Background(), v1.ListOptions{})
+	fmt.Println("RAW PSP, ERR", psp, err)
 
 	watchOptions := kubernetes.WatchOptions{
 		SyncTimeout: interval,
 		Namespace:   "kube-system",
 	}
 
-	watcher, err := kubernetes.NewWatcher(client, &kubernetes.Pod{}, watchOptions, nil)
+	watcher, err := kubernetes.NewWatcher(client, &policy.PodSecurityPolicy{}, watchOptions, nil)
 	if err != nil {
-		return nil, fmt.Errorf("error creating k8s client set: %v", err)
+		return nil, fmt.Errorf("could not create watcher: %w", err)
+	}
+
+	if err := watcher.Start(); err != nil {
+		return nil, fmt.Errorf("could not start watcher: %w", err)
 	}
 
 	logp.Info("Watcher initiated.")
+
+	fmt.Println("KUBE API REGISTERED")
 
 	return &KubeFetcher{
 		watcher: watcher,
@@ -40,21 +52,27 @@ func NewKubeFetcher(kubeconfig string, interval time.Duration) (Fetcher, error) 
 }
 
 func (f *KubeFetcher) Fetch() ([]interface{}, error) {
-	pods := f.watcher.Store().List()
+	fmt.Println("KUBE API FETCH")
+	psp := f.watcher.Store().List()
 
-	for _, p := range pods {
-		pod, ok := p.(*kubernetes.Pod)
+	fmt.Println("PSP COUNT", len(psp))
+
+	for _, p := range psp {
+		psp, ok := p.(*policy.PodSecurityPolicy)
+
+		fmt.Println("PSP OK?", psp, ok)
+
 		if !ok {
-			logp.Info("could not convert to pod")
+			logp.Info("could not convert to PodSecurityPolicy")
 			continue
 		}
-		pod.SetManagedFields(nil)
-		pod.Status.Reset()
-		pod.Kind = "Pod" // see https://github.com/kubernetes/kubernetes/issues/3030
+		psp.SetManagedFields(nil)
+		psp.Kind = "PodSecurityPolicy" // see https://github.com/kubernetes/kubernetes/issues/3030. TODO(yashtewari): Does this still apply for PodSecurityPolicy?
 	}
 
-	return pods, nil
+	return psp, nil
 }
 
 func (f *KubeFetcher) Stop() {
+	f.watcher.Stop()
 }

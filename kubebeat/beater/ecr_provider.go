92615f4774770668ef90ec31dba5f1ea08709522
ofir lapid <ofir@build.security>
2021-12-15 17:56:27 +0200

    perparing for first code review- naming + small fixes

--- kubebeat/beater/ecr_provider.go
+++ kubebeat/beater/ecr_provider.go
@@ -7,13 +7,13 @@ import (
 	"github.com/elastic/beats/v7/libbeat/logp"
 )
 
-type ECRDataFetcher struct {
+type ECRProvider struct {
 }
 
 /// This method will return a maximum of 100 repository
 /// If we will ever wish to change it, DescribeRepositories returns results in paginated manner
 /// When repoNames is nil, it will describe all the existing repositories
-func (e ECRDataFetcher) DescribeRepositories(cfg aws.Config, ctx context.Context, repoNames []string) ([]ecr.Repository, error) {
+func (e ECRProvider) DescribeRepositories(cfg aws.Config, ctx context.Context, repoNames []string) ([]ecr.Repository, error) {
 	svc := ecr.New(cfg)
 	input := &ecr.DescribeRepositoriesInput{
 		RepositoryNames: repoNames,

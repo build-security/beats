98dc9484f8a666733afd92172347f6ccb665b104
ofir lapid <ofir@build.security>
2021-12-16 13:50:45 +0200

    refactoring and renaming

--- kubebeat/beater/aws_kube_fetcher.go
+++ kubebeat/beater/aws_kube_fetcher.go
@@ -2,6 +2,7 @@ package beater
 
 import (
 	"context"
+	"fmt"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/aws/external"
 	"github.com/aws/aws-sdk-go-v2/service/ecr"
@@ -9,9 +10,9 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/elasticloadbalancing"
 	"github.com/elastic/beats/v7/libbeat/common/kubernetes"
 	"github.com/elastic/beats/v7/libbeat/logp"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	k8s "k8s.io/client-go/kubernetes"
-	"log"
 	"strings"
 	"time"
 )
@@ -25,17 +26,18 @@ type AwsKubeFetcher struct {
 	elb         ELBProvider
 }
 
-func NewAwsKubeFetcher(kubeconfig string, clusterName string) Fetcher {
+func NewAwsKubeFetcher(kubeconfig string, clusterName string) (Fetcher, error) {
 
 	kubernetesClient, err := kubernetes.GetKubernetesClient(kubeconfig, kubernetes.KubeClientOptions{})
 	if err != nil {
-		log.Fatal(err)
+		return nil, fmt.Errorf("fail to get k8sclient client: %s", err.Error())
 	}
 
 	cfg, err := external.LoadDefaultAWSConfig()
 	if err != nil {
-		log.Fatal(err)
+		return nil, fmt.Errorf("fail init aws config: %s", err.Error())
 	}
+
 	ecr := ECRProvider{}
 	eks := EKSProvider{}
 	elb := ELBProvider{}
@@ -47,7 +49,7 @@ func NewAwsKubeFetcher(kubeconfig string, clusterName string) Fetcher {
 		eks:         eks,
 		elb:         elb,
 		clusterName: clusterName,
-	}
+	}, nil
 }
 
 func (f AwsKubeFetcher) Fetch() ([]interface{}, error) {
@@ -110,6 +112,7 @@ func (f AwsKubeFetcher) GetLoadBalancerDescriptions() ([]elasticloadbalancing.Lo
 	defer cancel()
 
 	// TODO - leader election
+	// Running on all namespaces
 	services, err := f.kubeClient.CoreV1().Services("").List(ctx, metav1.ListOptions{})
 	if err != nil {
 		logp.Err("Failed to get all services  - %+v", err)
@@ -137,7 +140,7 @@ func (f AwsKubeFetcher) GetLoadBalancerDescriptions() ([]elasticloadbalancing.Lo
 }
 
 // EKS benchmark 5.4.3 Ensure clusters are created with Private Nodes (Manual)
-func (f AwsKubeFetcher) GetNodeDescription() ([]interface{}, error) {
+func (f AwsKubeFetcher) GetNodeDescription() ([]v1.Node, error) {
 
 	ctx, cancel := context.WithTimeout(context.TODO(), 30*time.Second)
 	defer cancel()
@@ -148,13 +151,8 @@ func (f AwsKubeFetcher) GetNodeDescription() ([]interface{}, error) {
 		logp.Err("Failed to get all nodes information  - %+v", err)
 		return nil, err
 	}
-	nodesInfo := make([]interface{}, 0)
-	for _, node := range nodeList.Items {
 
-		nodesInfo = append(nodesInfo, node)
-	}
-
-	return nodesInfo, err
+	return nodeList.Items , nil
 }
 
 func (f AwsKubeFetcher) Stop() {

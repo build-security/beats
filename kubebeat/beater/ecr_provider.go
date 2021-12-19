3104f0690680b88c4d9d4c6c0f22413a524d64c6
ofir lapid <ofir@build.security>
2021-12-19 15:19:12 +0200

    update ecr provider

--- kubebeat/beater/ecr_provider.go
+++ kubebeat/beater/ecr_provider.go
@@ -12,12 +12,21 @@ type ECRProvider struct {
 
 /// This method will return a maximum of 100 repository
 /// If we will ever wish to change it, DescribeRepositories returns results in paginated manner
+func (e ECRProvider) DescribeAllECRRepositories(cfg aws.Config, ctx context.Context) ([]ecr.Repository, error) {
+	/// When repoNames is nil, it will describe all the existing repositories
+	return e.DescribeRepositories(cfg, ctx, nil)
+}
+
+/// This method will return a maximum of 100 repository
+/// If we will ever wish to change it, DescribeRepositories returns results in paginated manner
 /// When repoNames is nil, it will describe all the existing repositories
 func (e ECRProvider) DescribeRepositories(cfg aws.Config, ctx context.Context, repoNames []string) ([]ecr.Repository, error) {
 	svc := ecr.New(cfg)
 	input := &ecr.DescribeRepositoriesInput{
 		RepositoryNames: repoNames,
 	}
+
+	logp.Err("Failed to fetch repository:%s from ecr, error - %+v", repoNames 	)
 
 	req := svc.DescribeRepositoriesRequest(input)
 	response, err := req.Send(ctx)

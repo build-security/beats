953e92d87e37dc90e099ca08f221ead8abaca3b9
Yash Tewari <yashtewari1996@gmail.com>
2021-12-20 18:42:00 +0530

    Different Kube resources are watched on different namespaces.

--- kubebeat/beater/kube.go
+++ kubebeat/beater/kube.go
@@ -2,22 +2,66 @@ package beater
 
 import (
 	"fmt"
-	"sync"
 	"time"
 
 	"github.com/elastic/beats/v7/libbeat/common/kubernetes"
 	"github.com/elastic/beats/v7/libbeat/logp"
 	"k8s.io/apimachinery/pkg/runtime"
+	k8s "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
 )
 
 const (
 	kubeSystemNamespace = "kube-system"
+	allNamespaces       = "" // The Kube API treats this as "all namespaces"
 )
 
 var (
-	watcherlock sync.Once
+	// vanillaClusterResources represents those resources that are required for a vanilla
+	// Kubernetes cluster.
+	vanillaClusterResources = []requiredResource{
+		{
+			&kubernetes.Pod{},
+			kubeSystemNamespace,
+		},
+		{
+
+			&kubernetes.Secret{},
+			allNamespaces,
+		},
+		{
+			&kubernetes.Role{},
+			allNamespaces,
+		},
+		{
+			&kubernetes.RoleBinding{},
+			allNamespaces,
+		},
+		{
+			&kubernetes.ClusterRole{},
+			allNamespaces,
+		},
+		{
+			&kubernetes.ClusterRoleBinding{},
+			allNamespaces,
+		},
+		{
+			&kubernetes.PodSecurityPolicy{},
+			allNamespaces,
+		},
+		// TODO(yashtewari): Problem: github.com/elastic/beats/vendor/k8s.io/apimachinery/pkg/api/errors/errors.go#401
+		// > "the server could not find the requested resource"
+		// {
+		// 	&kubernetes.NetworkPolicy{},
+		// 	allNamespaces,
+		// },
+	}
 )
+
+type requiredResource struct {
+	resource  kubernetes.Resource
+	namespace string
+}
 
 type KubeFetcher struct {
 	kubeconfig string
@@ -25,22 +69,36 @@ type KubeFetcher struct {
 	watchers   []kubernetes.Watcher
 }
 
-func NewKubeFetcher(kubeconfig string, interval time.Duration) Fetcher {
-	return &KubeFetcher{
+func NewKubeFetcher(kubeconfig string, interval time.Duration) (Fetcher, error) {
+	f := &KubeFetcher{
 		kubeconfig: kubeconfig,
 		interval:   interval,
 		watchers:   make([]kubernetes.Watcher, 0),
 	}
+
+	if err := f.initWatchers(); err != nil {
+		return nil, err
+	}
+
+	return f, nil
 }
 
-func (f *KubeFetcher) initWatcher(w kubernetes.Watcher, err error) error {
+func (f *KubeFetcher) initWatcher(client k8s.Interface, r requiredResource) error {
+	w, err := kubernetes.NewWatcher(client, r.resource, kubernetes.WatchOptions{
+		SyncTimeout: f.interval,
+		Namespace:   r.namespace,
+	}, nil)
 	if err != nil {
 		return fmt.Errorf("could not create watcher: %w", err)
 	}
 
 	// TODO(yashtewari): it appears that Start never returns in case of certain failures, for example
-	// if the configured client role does not have the necessary permissions to list the
-	// resource being watched. This needs to be handled.
+	// if the configured Client's Role does not have the necessary permissions to list the Resource
+	// being watched. This needs to be handled.
+	//
+	// When such a failure happens, kubebeat won't shut down gracefuly, i.e. Stop will not work. This
+	// happens due to a context.TODO present in the libbeat dependency. It needs to accept context
+	// from the caller instead.
 	if err := w.Start(); err != nil {
 		return fmt.Errorf("could not start watcher: %w", err)
 	}
@@ -60,25 +118,8 @@ func (f *KubeFetcher) initWatchers() error {
 
 	f.watchers = make([]kubernetes.Watcher, 0)
 
-	watchOptions := kubernetes.WatchOptions{
-		SyncTimeout: f.interval,
-		Namespace:   kubeSystemNamespace,
-	}
-
-	for _, r := range []kubernetes.Resource{
-		&kubernetes.Pod{},
-		&kubernetes.Secret{},
-		&kubernetes.Role{},
-		&kubernetes.RoleBinding{},
-		&kubernetes.ClusterRole{},
-		&kubernetes.ClusterRoleBinding{},
-		&kubernetes.PodSecurityPolicy{},
-		// TODO(yashtewari): Problem: github.com/elastic/beats/vendor/k8s.io/apimachinery/pkg/api/errors/errors.go#401
-		// > "the server could not find the requested resource"
-		//
-		// &kubernetes.NetworkPolicy{},
-	} {
-		err := f.initWatcher(kubernetes.NewWatcher(client, r, watchOptions, nil))
+	for _, r := range vanillaClusterResources {
+		err := f.initWatcher(client, r)
 		if err != nil {
 			return err
 		}
@@ -90,16 +131,6 @@ func (f *KubeFetcher) initWatchers() error {
 }
 
 func (f *KubeFetcher) Fetch() ([]interface{}, error) {
-	var err error
-	watcherlock.Do(func() {
-		err = f.initWatchers()
-	})
-	if err != nil {
-		// Reset because not initialised properly.
-		watcherlock = sync.Once{}
-		return nil, err
-	}
-
 	ret := make([]interface{}, 0)
 
 	for _, w := range f.watchers {

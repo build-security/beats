498421eca532b970388306b1ae2cee022a2dc1f6
ofir lapid <ofir@build.security>
2021-12-21 17:31:07 +0200

    fix typos

--- kubebeat/beater/ecr_fetcher.go
+++ kubebeat/beater/ecr_fetcher.go
@@ -20,7 +20,7 @@ func NewECRFetcher(cfg aws.Config) (Fetcher, error) {
 func (f ECRFetcher) Fetch() ([]interface{}, error) {
 	results := make([]interface{}, 0)
 	ctx := context.Background()
-	// TODO - The provider should get a list of the repository it needs to check, and not check the entire ECR account`
+	// TODO - The provider should get a list of the repositories it needs to check, and not check the entire ECR account`
 	repositories, err := f.ecrProvider.DescribeAllECRRepositories(ctx)
 	results = append(results, repositories)
 

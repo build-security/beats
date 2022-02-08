534af19f4f95d5e4f96b20cc4b71206884993451
Amir Ben Nun <amir.bennun@elastic.co>
2022-02-08 17:39:23 +0200

    revert main.go

--- cloudbeat/main.go
+++ cloudbeat/main.go
@@ -1,25 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
-	"github.com/elastic/beats/v7/cloudbeat/beater"
 	"github.com/elastic/beats/v7/cloudbeat/cmd"
-	"github.com/elastic/beats/v7/libbeat/cmd/instance"
 
 	_ "github.com/elastic/beats/v7/cloudbeat/include"
 )
 
-func main1() {
+func main() {
 	if err := cmd.RootCmd.Execute(); err != nil {
 		os.Exit(1)
-	}
-}
-
-func main() {
-	err := instance.Run(instance.Settings{Name: cmd.Name}, beater.New)
-	if err != nil {
-		fmt.Printf("Main Beat Error %v", err)
 	}
 }
